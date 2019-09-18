46a7a98add842fa5ecf3337151d939568c76a680
ASquirrelsTail <rjtwilton@hotmail.com>
2019-09-18 20:40:54 +0100

    Expanded and added tests for ticket model for new ticket_type field, and vote method.

--- tickets/test_models.py
+++ tickets/test_models.py
@@ -1,8 +1,9 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from tickets.models import Ticket, Comment, Vote
 from django.utils import timezone
 from datetime import timedelta
+from tickets.models import Ticket, Comment, Vote
+from credits.models import Wallet
 
 
 class TicketModelTestCase(TestCase):
@@ -14,23 +15,32 @@ class TicketModelTestCase(TestCase):
         cls.test_user = User.objects.create_user(username='TestUser', email='test@test.com',
                                                  password='tH1$isA7357')
 
-        test_ticket = Ticket(user=cls.test_user, title='Test title', ticket_type='Bug', content='Test content')
-        test_ticket.save()
-        test_ticket.set_status('approved')
+        cls.bug_ticket = Ticket(user=cls.test_user, title='Test Bug', ticket_type='Bug', content='Test content')
+        cls.bug_ticket.save()
+        cls.bug_ticket.set_status('approved')
+
+        cls.feature_ticket = Ticket(user=cls.test_user, title='Test Feature', ticket_type='Feature', content='Test content')
+        cls.feature_ticket.save()
+        cls.feature_ticket.set_status('approved')
+
+# TEST TICKET PROPERTIES #
 
     def test_ticket_str_is_ticket_no_dash_title_dash_type(self):
         '''
         Test the ticket str name is of the format '{ticket number} - {title} - {ticket type}'
         '''
-        ticket = Ticket.objects.get(id=1)
-        self.assertEqual(str(ticket), '1 - Test title - Bug Report')
+        ticket = Ticket.objects.get(id=self.bug_ticket.id)
+        self.assertEqual(str(ticket), '1 - Test Bug - Bug Report')
+
+        ticket = Ticket.objects.get(id=self.feature_ticket.id)
+        self.assertEqual(str(ticket), '2 - Test Feature - Feature Request')
 
     def test_absolute_url_returns_ticket_detail(self):
         '''
         Test the absolute url returns an existing page, and that page uses
         the ticket_detail template and holds the ticket in its context.
         '''
-        ticket = Ticket.objects.get(id=1)
+        ticket = Ticket.objects.get(id=self.bug_ticket.id)
         url = ticket.get_absolute_url()
         response = self.client.get(str(url))
         self.assertEqual(response.status_code, 200)
@@ -38,13 +48,10 @@ class TicketModelTestCase(TestCase):
         self.assertEqual(response.context['object'], ticket)
 
     def test_noun_returns_verbose_ticket_type(self):
-        ticket = Ticket.objects.get(id=1)
+        ticket = Ticket.objects.get(id=self.bug_ticket.id)
         self.assertEqual(ticket.noun, 'Bug Report')
 
-        feature_ticket = Ticket(user=self.test_user, title='Feature', ticket_type='Feature', content='Test content')
-        feature_ticket.save()
-
-        feature_ticket = Ticket.objects.get(id=feature_ticket.id)
+        feature_ticket = Ticket.objects.get(id=self.feature_ticket.id)
         self.assertEqual(feature_ticket.noun, 'Feature Request')
 
     def test_no_views_returns_correct_numbers(self):
@@ -65,20 +72,27 @@ class TicketModelTestCase(TestCase):
         '''
         Test the no_votes property returns the correct number of votes.
         '''
-        test_ticket = Ticket(user=self.test_user, title='Votes', content='Test content')
-        test_ticket.save()
+        test_ticket_bug = Ticket(user=self.test_user, title='Bug Votes', content='Test content')
+        test_ticket_bug.save()
+
+        test_ticket_feature = Ticket(user=self.test_user, title='Feature Votes', content='Test content')
+        test_ticket_feature.save()
 
         for number in range(3):
             voter = User.objects.create_user(username='VoteUser{}'.format(number),
                                              email='test{}@test.com'.format(number),
                                              password='tH1$isA7357')
-            vote = Vote(user=voter, ticket=test_ticket)
-            vote.save()
+            bug_vote = Vote(user=voter, ticket=test_ticket_bug)
+            bug_vote.save()
+            feature_vote = Vote(user=voter, ticket=test_ticket_feature, count=3)
+            feature_vote.save()
 
-        ticket_zero_votes = Ticket.objects.get(id=1)
+        ticket_zero_votes = Ticket.objects.get(id=self.bug_ticket.id)
         self.assertEqual(0, ticket_zero_votes.no_votes)
-        ticket_three_votes = Ticket.objects.get(id=test_ticket.id)
+        ticket_three_votes = Ticket.objects.get(id=test_ticket_bug.id)
         self.assertEqual(3, ticket_three_votes.no_votes)
+        ticket_nine_votes = Ticket.objects.get(id=test_ticket_feature.id)
+        self.assertEqual(9, ticket_nine_votes.no_votes)
 
     def test_comments_returns_primary_comments(self):
         '''
@@ -151,6 +165,8 @@ class TicketModelTestCase(TestCase):
         test_ticket = Ticket.objects.get(id=test_ticket.id)
         self.assertEqual('done', test_ticket.status)
 
+# TEST TICKET HAS_VOTED METHOD#
+
     def test_has_voted_returns_false_if_user_has_not_voted(self):
         '''
         Test that the has_voted method returns false if a user has not voted for the ticket.
@@ -173,6 +189,180 @@ class TicketModelTestCase(TestCase):
 
         test_ticket = Ticket.objects.get(id=test_ticket.id)
         self.assertTrue(test_ticket.has_voted(voter))
+
+# TEST TICKET VOTE METHOD FOR BUG TICKET TYPE #
+
+    def test_vote_for_bug_increases_votes(self):
+        '''
+        Test that the vote method increases votes on bug type tickets.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Bug', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+
+        test_ticket.vote(voter)
+        test_ticket = Ticket.objects.get(id=test_ticket.id)
+        self.assertEqual(1, test_ticket.no_votes)
+
+    def test_vote_for_bug_returns_success_and_message(self):
+        '''
+        Test that voting for a bug returns success as True, and an appropriate message.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Bug', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+
+        result = test_ticket.vote(voter)
+        self.assertTrue(result['success'])
+        self.assertIn('success', result['message'].lower())
+
+    def test_vote_for_bug_twice_does_not_increase_votes_a_second_time(self):
+        '''
+        Test voting for a bug a second time does not increase the number of votes.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Bug', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+
+        test_ticket.vote(voter)
+        test_ticket.vote(voter)
+        test_ticket = Ticket.objects.get(id=test_ticket.id)
+        self.assertEqual(1, test_ticket.no_votes)
+
+    def test_vote_for_bug_twice_returns_failiure_and_message(self):
+        '''
+        Test that voting for a bug a second time returns success as False, and an appropriate message.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Bug', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+
+        test_ticket.vote(voter)
+        result = test_ticket.vote(voter)
+        self.assertFalse(result['success'])
+        self.assertIn('already voted', result['message'].lower())
+
+# TEST TICKET VOTE METHOD FOR FEATURE TICKET TYPE #
+
+    def test_vote_for_feature_increases_votes_by_credits_spent(self):
+        '''
+        Test that the vote method increases votes on feature type tickets by the number of credits spent.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Feature', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+        wallet = Wallet(user=voter)
+        wallet.save()
+        wallet.credit(10)
+
+        test_ticket.vote(voter, 5)
+        test_ticket = Ticket.objects.get(id=test_ticket.id)
+        self.assertEqual(5, test_ticket.no_votes)
+
+    def test_vote_for_feature_returns_success_and_message(self):
+        '''
+        Test that voting for a feature returns success as True, and an appropriate message.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Feature', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+        wallet = Wallet(user=voter)
+        wallet.save()
+        wallet.credit(10)
+
+        result = test_ticket.vote(voter, 5)
+        self.assertTrue(result['success'])
+        self.assertIn('success', result['message'].lower())
+
+    def test_vote_for_feature_reduces_wallet_balance_by_credits_spent(self):
+        '''
+        Test that voting for a feature debits the user the correct number of credits.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Feature', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+        wallet = Wallet(user=voter)
+        wallet.save()
+        wallet.credit(10)
+
+        test_ticket.vote(voter, 5)
+        voter_wallet = Wallet.objects.get(id=wallet.id)
+        self.assertEqual(5, voter_wallet.balance)
+
+    def test_vote_for_feature_twice_increase_votes_a_second_time(self):
+        '''
+        Test voting for a feature a second time again increase the number of votes by the number of crdits spent.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Feature', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+        wallet = Wallet(user=voter)
+        wallet.save()
+        wallet.credit(10)
+
+        test_ticket.vote(voter, 5)
+        test_ticket.vote(voter, 3)
+        test_ticket = Ticket.objects.get(id=test_ticket.id)
+        self.assertEqual(8, test_ticket.no_votes)
+
+    def test_vote_for_feature_no_credits_returns_failiure_and_message(self):
+        '''
+        Test that voting for a feature with an empty of non-existent wallet returns success as False,
+        and an appropriate message.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Feature', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+
+        result = test_ticket.vote(voter, 5)
+        self.assertFalse(result['success'])
+        self.assertIn('insufficient credits', result['message'].lower())
+
+        wallet = Wallet(user=voter)
+        wallet.save()
+
+        result = test_ticket.vote(voter, 5)
+        self.assertFalse(result['success'])
+        self.assertIn('insufficient credits', result['message'].lower())
+
+    def test_vote_for_feature_insufficient_credits_returns_failiure_and_message(self):
+        '''
+        Test that voting for a feature with an empty of non-existent wallet returns success as False,
+        and an appropriate message.
+        '''
+        test_ticket = Ticket(user=self.test_user, title='Voting', ticket_type='Feature', content='Test content')
+        test_ticket.save()
+
+        voter = User.objects.create_user(username='VotingUser', email='test@test.com',
+                                         password='tH1$isA7357')
+
+        wallet = Wallet(user=voter)
+        wallet.save()
+        wallet.credit(10)
+
+        result = test_ticket.vote(voter, 15)
+        self.assertFalse(result['success'])
+        self.assertIn('insufficient credits', result['message'].lower())
+
+# TEST TICKET SET_STATUS METHOD #
 
     def test_set_status_changes_status(self):
         '''
@@ -275,7 +465,7 @@ class CommentModelTestCase(TestCase):
 
     def test_comment_replies_contains_replies(self):
         '''
-        Test replies property returns a comments replies.
+        Test replies property returns a comment's replies.
         '''
         comment = Comment.objects.get(id=self.comment2.id)
         self.assertIn(self.reply1, comment.replies)
